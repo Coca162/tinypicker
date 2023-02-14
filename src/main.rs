acf0a3c504a955f9bee58ab4c70b00ad3a91ede9
Coca162 <coca16622@gmail.com>
2023-02-14 21:27:59 +0000

    Return on clipboard bin success

--- src/main.rs
+++ src/main.rs
@@ -112,6 +112,8 @@ fn send_to_clibpoard(rgb_hex: &str) {
 
     if let Err(err) = bin_result {
         eprintln!("{err} Could not use xclip, attempting fork");
+    } else {
+        return;
     }
 
     let fork_result =
